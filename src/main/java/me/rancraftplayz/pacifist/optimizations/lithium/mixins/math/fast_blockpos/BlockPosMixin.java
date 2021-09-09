2664c715836988f68787b30e95d11ada29dc7400
RanCraftPlayz <43445785+RanCraftPlayz@users.noreply.github.com>
2021-09-09 11:07:05 +0600

    prototyoe remapper which can remap mixins

--- src/main/java/me/rancraftplayz/pacifist/optimizations/lithium/mixins/math/fast_blockpos/BlockPosMixin.java
+++ src/main/java/me/rancraftplayz/pacifist/optimizations/lithium/mixins/math/fast_blockpos/BlockPosMixin.java
@@ -1,7 +1,8 @@
 package me.rancraftplayz.pacifist.optimizations.lithium.mixins.math.fast_blockpos;
 
+import net.minecraft.core.BaseBlockPosition;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Vec3i;
+import net.minecraft.core.BlockPosition;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -10,8 +11,8 @@ import org.spongepowered.asm.mixin.Overwrite;
  * you can find the original code on https://github.com/CaffeineMC/lithium-fabric/ (Yarn mappings)
  */
 
-@Mixin(BlockPos.class)
-public abstract class BlockPosMixin extends Vec3i {
+@Mixin(value = BlockPosition.class, remap = false)
+public abstract class BlockPosMixin extends BaseBlockPosition {
     public BlockPosMixin(int x, int y, int z) {
         super(x, y, z);
     }
@@ -20,107 +21,107 @@ public abstract class BlockPosMixin extends Vec3i {
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos up() {
-        return new BlockPos(this.getX(), this.getY() + 1, this.getZ());
+    @Overwrite(remap = false)
+    public BlockPosition up() {
+        return (BlockPosition) (Object) new BlockPos(this.getX(), this.getY() + 1, this.getZ());
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos up(int distance) {
-        return new BlockPos(this.getX(), this.getY() + distance, this.getZ());
+    @Overwrite(remap = false)
+    public BlockPosition up(int distance) {
+        return (BlockPosition) (Object) new BlockPos(this.getX(), this.getY() + distance, this.getZ());
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos down() {
-        return new BlockPos(this.getX(), this.getY() - 1, this.getZ());
+    @Overwrite(remap = false)
+    public BlockPosition down() {
+        return (BlockPosition) (Object) new BlockPos(this.getX(), this.getY() - 1, this.getZ());
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos down(int distance) {
-        return new BlockPos(this.getX(), this.getY() - distance, this.getZ());
+    @Overwrite(remap = false)
+    public BlockPosition down(int distance) {
+        return (BlockPosition) (Object) new BlockPos(this.getX(), this.getY() - distance, this.getZ());
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos north() {
-        return new BlockPos(this.getX(), this.getY(), this.getZ() - 1);
+    @Overwrite(remap = false)
+    public BlockPosition north() {
+        return (BlockPosition) (Object) new BlockPos(this.getX(), this.getY(), this.getZ() - 1);
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos north(int distance) {
-        return new BlockPos(this.getX(), this.getY(), this.getZ() - distance);
+    @Overwrite(remap = false)
+    public BlockPosition north(int distance) {
+        return (BlockPosition) (Object) new BlockPos(this.getX(), this.getY(), this.getZ() - distance);
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos south() {
-        return new BlockPos(this.getX(), this.getY(), this.getZ() + 1);
+    @Overwrite(remap = false)
+    public BlockPosition south() {
+        return (BlockPosition) (Object) new BlockPos(this.getX(), this.getY(), this.getZ() + 1);
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos south(int distance) {
-        return new BlockPos(this.getX(), this.getY(), this.getZ() + distance);
+    @Overwrite(remap = false)
+    public BlockPosition south(int distance) {
+        return (BlockPosition) (Object) new BlockPos(this.getX(), this.getY(), this.getZ() + distance);
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos west() {
-        return new BlockPos(this.getX() - 1, this.getY(), this.getZ());
+    @Overwrite(remap = false)
+    public BlockPosition west() {
+        return (BlockPosition) (Object) new BlockPos(this.getX() - 1, this.getY(), this.getZ());
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos west(int distance) {
-        return new BlockPos(this.getX() - distance, this.getY(), this.getZ());
+    @Overwrite(remap = false)
+    public BlockPosition west(int distance) {
+        return (BlockPosition) (Object) new BlockPos(this.getX() - distance, this.getY(), this.getZ());
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos east() {
-        return new BlockPos(this.getX() + 1, this.getY(), this.getZ());
+    @Overwrite(remap = false)
+    public BlockPosition east() {
+        return (BlockPosition) (Object) new BlockPos(this.getX() + 1, this.getY(), this.getZ());
     }
 
     /**
      * @author JellySquid
      * @reason Simplify and inline
      */
-    @Overwrite
-    public BlockPos east(int distance) {
-        return new BlockPos(this.getX() + distance, this.getY(), this.getZ());
+    @Overwrite(remap = false)
+    public BlockPosition east(int distance) {
+        return (BlockPosition) (Object) new BlockPos(this.getX() + distance, this.getY(), this.getZ());
     }
 }
